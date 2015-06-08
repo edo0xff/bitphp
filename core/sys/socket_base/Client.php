e11b5a8a4835effb4b44d6dd0f6d39b35142da42
0oeduardoo0 <ms7rbeta@gmail.com>
2015-06-08 12:10:28 -0500

    Nuevas caracteristicas en el servidor de sockets

--- core/sys/socket_base/Client.php
+++ core/sys/socket_base/Client.php
@@ -11,8 +11,22 @@
             socket_getpeername($socket, $this->ip);
         }
 
-        public function set($var, $val) {
-            $this->$var = $val;
+        public function __call( $methodName, array $args ) {
+            if( isset( $this->newFunctions[ $methodName ] ) ) {
+                return call_user_func_array( $this->newFunctions[ $methodName ], $args );
+            }
+
+            throw new RunTimeException("El metodo $methodName() no existe dentro de la instancia de la aplicacion.");
+        }
+
+        public function set( $item, $value ) {
+            
+            if( !is_callable( $value ) ) {
+                $this->$item = $value;
+                return 1;
+            }
+
+            $this->newFunctions[ $item ] = Closure::bind( $value, $this, get_class() );
         }
 
         public function send($message) {
